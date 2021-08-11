abc58908724d00da560e80d7745296750f77e9c9
Ashish Kiran <ashishkiran93@gmail.com>
2021-08-11 19:37:14 +0530

    seventh commit

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV == "production"){
     const path = require('path')
     
     app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname,"client", "index.html"))
     })
 }
 
